11e8447b3d37d2f74eee7be5e2bb1b8d8fdc4230
BEN <freefire1379045919@gmail.com>
2025-07-23 22:54:02 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "5947904339"
     sudo_users = "5947904339"
-    GROUP_ID = -4970580235
+    GROUP_ID = -1002081085931
     TOKEN = "8069497597:AAHCG3qd9I4H5awf7uogQ3pVMEkoVrjbc-0"
     mongo_url = "mongodb+srv://freefire1379045919:ZJrcwCD0D1FIMMzz@cluster22.f1ro5nh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster22"
     PHOTO_URL = ["https://graph.org/file/f181c8be7b22c4198e712-47f15e0688dc6f742e.jpg", "https://graph.org/file/68bc036f99234e59ae255-e7302854249889d963.jpg"]
